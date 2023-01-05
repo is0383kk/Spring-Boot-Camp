411011530637283c83e31761fd1290eb4f211cf7
Yoshiwo <is0383kk@gmail.com>
2023-01-05 11:50:07 +0900

    Add files via upload

---
+++ Chap5_BusinessLogic/spring-business-logic/src/test/java/com/example/springbusinesslogic/SpringBusinessLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbusinesslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBusinessLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
